a3bcd52bbce4374fdeaba621ae0bd675a676798a
Crowdbotics <team@crowdbotics.com>
2020-04-15 20:18:04 +0000

    Update models

--- backend/ipills_15887/wsgi.py
+++ backend/ipills_15887/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipills_15887.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ipills_15887.settings")
 
 application = get_wsgi_application()
